d06c636920639d2d4ef4aa76b7077485d548098e
CosimoTramonte <cosimotramonte7@gmail.com>
2023-03-22 14:50:46 +0100

    Add Dom with style

--- js/script.js
+++ js/script.js
@@ -1,13 +1,24 @@
+
+const container = document.querySelector(".container");
+
 //Ciclio for 1-100
 for(let i = 1; i <= 100; i++){
     console.log(i)
 
+    const box= document.createElement("div")
 
+    box.classList.add("box")
+
+    //Ciclo with multiple
     if(i % 3 === 0 && i % 5 === 0){
-        console.log("FizzBuzz")
+        box.classList.add("multiple35")
     } else if (i % 3 === 0){
-        console.log("Fizz")
+        box.classList.add("multiple3")
     } else if (i % 5 === 0){
-        console.log("Buzz")
-    }
+        box.classList.add("multiple5")
+    } 
+
+    box.append(i)
+
+    container.append(box)
 }
